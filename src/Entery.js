ef5c46d0b1cc0accc68c76c2499e377e08029b3a
srh0820 <srh96820@gmail.com>
2024-07-16 11:27:02 +0900

    useState

--- src/Entery.js
+++ src/Entery.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Entery() {
+    let count = 0;  
+    // 일반 변수는 상태변수 값을 받아서 출력하려면 return 출력식에서 처리하기
+    const [num, setNum] = useState(0);
     return (
-        <h1>첫페이지 컨텐츠</h1>
+        <div>
+            <button onClick={
+                        ()=>{ 
+                            setNum(num + 1) // 상태변수의 값을 변경하는 상태함수
+                            // count = num; XX 함수 내에서는 렌더링되지 않음 
+                        }
+                       }>일반변수 count 하나씩 증가</button>
+            <h1>일반변수 count의 상태변수 값을 대입해서 출력 : { `${count = num}` } </h1>
+        </div>        
     )
 }
 
