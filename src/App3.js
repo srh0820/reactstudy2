06430216a2fc415419fff4d6530d2eb4dcb18ea4
srh0820 <srh96820@gmail.com>
2024-07-18 12:33:45 +0900

    useEffect

--- src/App3.js
+++ src/App3.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import tabjson from './json/tab.json'
 
 function App3() {
     const [ tabnm, setTabnm ] = useState('notice');
+
+    useEffect(()=>{
+        const buttons = document.querySelectorAll('.tabs button'); 
+        let currentIndex = 0;
+
+        const switchTab = () => {
+            buttons[currentIndex].click();  // buttons => array 형 정렬형 데이터
+            currentIndex = (currentIndex + 1) % buttons.length;
+        };
+        const interval = setInterval(switchTab, 3000);
+
+        return () => clearInterval(interval);   // 언마운트 - 이 컴포넌트가 삭제될 때
+        
+    }, [])
 
     return (
         <div className="tapwrap d-flex container border">
